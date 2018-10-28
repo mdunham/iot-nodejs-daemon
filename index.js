c3f423c54eefa30927311e7715c944802535a374
Matthew Dunham <matt@matthewdunham.net>
2018-10-28 14:52:17 -0500

    optimized the main init script it was getting too long

--- index.js
+++ index.js
@@ -1,144 +1,35 @@
 /**
- * Initialization script for the Cleveland-LCR Daemon
+ * Main init script for the Cleveland-LCR Daemon.
  * 
- * useage: node index.js [--debug=true|false] [--lcrCom=ttyUSB0]
- * flags: 
- *     debug - Will cause the script to run verbose.
- *     lcrCom - The com device for the LCR Meter. Defaults to ttyUSB0.
+ * useage: ./bin/cl-lcr-cli [start|stop|daemon] [start|stop]
+ * useage: ./bin/cl-lcr-cli [register|unregister]
  *     
  * @author Matthew Dunham <matt@hotcoffeydesign.com>
  */
 
 "use strict";
 
-// Serial Comms Demo
+const sqlite3 = require('sqlite3');
+const lcr = require('./lib/lcr.js');
+const ble = require('./lib/ble.js');
 
-let lcr = require('./lib/lcr.js');
+let db = new sqlite3.Database('./db/lcr-cl.db', (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the SQLite database.');
+});
 
 let device = new lcr('ttyUSB0', 250, 255);
+let server = new ble(device);
 
-setTimeout(() => {
-	console.log('interval...');
-	device.checkStatus((status, productID, productName) => {
-		if (status) {
-			console.log('----- STATUS CHECK PASS -----');
-			console.log('ProductID: ' + productID + ' - Product Name: ' + productName);
-		} else {
-			console.log('----- STATUS CHECK FAIL -----');
-		}
+// TODO: Add sqlite database for collecting data to send over the internet
+// TODO: GeoTracking & Security
+var field = 0;
+setInterval(() => {
+	field++;
+	lcr.getField(field, (status, response) => {
+		if (status) console.log('Get Field Success!!!!!', response);
+		else console.log('Get Field Said FALSE!!');
 	});
-}, 2000);
-
-/***
-const bleno = require("bleno");
-
-console.log("Starting bleno...");
-
-const COUNTER_SERVICE_UUID = "00010000-9FAB-43C8-9231-40F6E305F96D";
-const COUNTER_CHAR_UUID = "00010001-9FAB-43C8-9231-40F6E305F96D";
-
-
-class CounterCharacteristic extends bleno.Characteristic {
-    constructor() {
-        super({
-            uuid: COUNTER_CHAR_UUID,
-            properties: ["notify"],
-            value: null
-        });
-
-        this.counter = 0;
-    }
-
-    onSubscribe(maxValueSize, updateValueCallback) {
-        console.log(`Counter subscribed, max value size is ${maxValueSize}`);
-        this.updateValueCallback = updateValueCallback;
-    }
-
-    onUnsubscribe() {
-        console.log("Counter unsubscribed");
-        this.updateValueCallback = null;
-    }    
-
-    sendNotification(value) {
-        if(this.updateValueCallback) {
-            console.log(`Sending notification with value ${value}`);
-
-            const notificationBytes = new Buffer(2);
-            notificationBytes.writeInt16LE(value);
-
-            this.updateValueCallback(notificationBytes);
-        }
-    }
-
-    start() {
-        console.log("Starting counter");
-        this.handle = setInterval(() => {
-            this.counter = (this.counter + 1) % 0xFFFF;
-            this.sendNotification(this.counter);
-        }, 1000);
-    }
-
-    stop() {
-        console.log("Stopping counter");
-        clearInterval(this.handle);
-        this.handle = null;
-    }
-}
-
-let counter = new CounterCharacteristic();
-counter.start();
-
-
-bleno.on("stateChange", state => {
-
-    if (state === "poweredOn") {
-        
-        bleno.startAdvertising("Counter", [COUNTER_SERVICE_UUID], err => {
-            if (err) console.log(err);
-        });
-
-    } else {
-        console.log("Stopping...");
-        counter.stop();
-        bleno.stopAdvertising();
-    }        
-});
-
-bleno.on("advertisingStart", err => {
-
-    console.log("Configuring services...");
-    
-    if(err) {
-        console.error(err);
-        return;
-    }
-
-    let service = new bleno.PrimaryService({
-        uuid: COUNTER_CHAR_UUID,
-        characteristics: [counter]
-    });
-
-    bleno.setServices([service], err => {
-        if(err)
-            console.log(err);
-        else
-            console.log("Services configured");
-    });
-});
-
-// some diagnostics 
-bleno.on("stateChange", state => console.log(`Bleno: Adapter changed state to ${state}`));
-
-bleno.on("advertisingStart", err => console.log("Bleno: advertisingStart"));
-bleno.on("advertisingStartError", err => console.log("Bleno: advertisingStartError"));
-bleno.on("advertisingStop", err => console.log("Bleno: advertisingStop"));
-
-bleno.on("servicesSet", err => console.log("Bleno: servicesSet"));
-bleno.on("servicesSetError", err => console.log("Bleno: servicesSetError"));
-
-bleno.on("accept", clientAddress => console.log(`Bleno: accept ${clientAddress}`));
-bleno.on("disconnect", clientAddress => console.log(`Bleno: disconnect ${clientAddress}`));
-
-// End of Bluetooth Dmeo
-
-*/
+}, 500);
