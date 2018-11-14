1edfe5b1767e3a74b8322d2796b410139d9c9344
Matthew Dunham <matt@matthewdunham.net>
2018-11-14 14:51:07 -0600

    fixed an issue with boot order

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "$truckID" > /etc/cl-lcr-truck
 cp /etc/rc.local /etc/rc.backup
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "hciconfig hci0 up" >> /etc/rc.local
-echo "stty -F /dev/ttyAMA0 9600 &" >> /etc/rc.local
+echo "stty -F /dev/ttyAMA0 9600" >> /etc/rc.local
 echo "gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 
