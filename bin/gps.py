7590f5cc6d06d7931c2deeb06f80e47498827457
Matthew Dunham <matt@matthewdunham.net>
2018-11-13 20:14:31 -0600

    changed the var str to gstr it was overriding str()

--- bin/gps.py
+++ bin/gps.py
@@ -3,32 +3,20 @@ import pynmea2
 
 from Hologram.HologramCloud import HologramCloud
 
-str = ''
-#Get GPS Location
+gstr = ''
 serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=5)
 
-#Wait until we have a valid location
-while str.find('GGA') == -1:
-    str = serialPort.readline()
+while gstr.find('GGA') == -1:
+    gstr = serialPort.readline()
 
-#parse the string for latitude and longitude
-msg = pynmea2.parse(str)
+msg = pynmea2.parse(gstr)
 print msg
 print msg.latitude
 print msg.longitude
 
-latitude = msg.latitude
-longitude = msg.longitude
+latitude = str(msg.latitude)
+longitude = str(msg.longitude)
 
-#Set the sign of the latitude based on the direction (N/S)
-#if msg.lat_dir == 'S':
-#    latitude = '-' + latitude
-
-#Set the sign of the longitude based on the direction (E/W)
-#if msg.lon_dir == 'W':
-#    longitude = '-' + longitude
-
-#Get a hologram instance and connect
 credentials = {'devicekey':'ujk{]5pX'}
 hologram = HologramCloud(credentials, network='cellular')
 
@@ -46,20 +34,11 @@ if location is None:
 	location = false
 	message = latitude+","+longitude
 else:
-	message = latitude+","+longitude+"|"+location.latitude+","+location.longitude
+	message = latitude+","+longitude+"|"+str(location.latitude)+","+str(location.longitude)
 
-if result == False:
-    print ' Failed to connect to cell network'
-
-#Our message will be a google maps link that will show the current location
-
-#print message
-
-#Send the message
 response_code = hologram.sendMessage(message,topics=["gmail"])
 
 hologram.getResultString(response_code)
 print message
 
-#Disconnect
 hologram.network.disconnect()
