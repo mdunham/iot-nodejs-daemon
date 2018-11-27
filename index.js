1fe6d987b878282e9b236a6ae695e03b461cc846
Matthew Dunham <matt@matthewdunham.net>
2018-11-27 00:04:14 -0600

    added totalizer to examples

--- index.js
+++ index.js
@@ -46,35 +46,37 @@ let
 					device.setField(0x25, [0x02], (status, deviceByte) => {
 						device.removeTransaction(() => { 
 							device.setField(0x10, [0x00], function(status, deviceByte, data) {
-							device.setField(0x5D, [0x00], function(status, deviceByte, data) {
-								var
-									_getFields = [0x02, 0x03, 0x04, 0x05, 0x06, 0x0D, 0x0E, 0x11, 0x12, 0x64, 0x65, 0x2C, 0x2D, 0x47, 0x5C, 0x5D], 
-									getFields = function(index) {
-										if (index == _getFields.length) return;
-										console.log('getfield called: ' + index);
-										var wId = setTimeout(function(){ getFields(++index); }, 1000);
-										device.getField(_getFields[index], function(status, deviceByte, data) {
-											let
-												totalizer,
-												buf = new ArrayBuffer(4),
-												view = new DataView(buf);
-											if (data && data.length) {
-												data.forEach(function (b, i) {
-													view.setUint8(i, parseInt(b, 16));
-												});
-
-												totalizer = view.getInt32(0, 1);
-												if (status) {
-													console.log('Volume ' + index + ' is: ' + totalizer);
+								device.setField(0x5D, [0x00, 0x00, 0x00, 0x00], function(status, deviceByte, data) {});
+								setTimeout(function(){
+									var
+										_getFields = [0x2E, 0x2F, 0x37, 0x02, 0x03, 0x04, 0x05, 0x06, 0x0D, 0x0E, 0x11, 0x12, 0x64, 0x65, 0x2C, 0x2D, 0x47, 0x5C, 0x5D], 
+										getFields = function(index) {
+											if (index == _getFields.length) return;
+											console.log('getfield called: ' + index);
+											var wId = setTimeout(function(){ getFields(++index); }, 1000);
+											device.getField(_getFields[index], function(status, deviceByte, data) {
+												let
+													totalizer,
+													buf = new ArrayBuffer(4),
+													view = new DataView(buf);
+												if (data && data.length) {
+													data.forEach(function (b, i) {
+														view.setUint8(i, parseInt(b, 16));
+													});
+													console.log('1: ' + view.getInt32(0, 1) + '  2: ' + view.getFloat32(0, 1) + '  3: ' + view.getInt32(0) + '  4: ' + view.getFloat32(0))
+													
+													if (status) {
+														//console.log('Volume ' + index + ' is: ' + totalizer);
+													}
 												}
-											}
-											clearTimeout(wId);
-											getFields(++index);
-										});
-									};
+												clearTimeout(wId);
+												getFields(++index);
+											});
+										};
 
-								getFields(0);
-							});});
+									getFields(0);
+								}, 1000);
+							});
 						});
 					});
 				});
